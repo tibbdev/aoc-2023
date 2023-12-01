77cf65cf522f8143ddf3f9ffac3b76db916489c2
M. Tibbitts <marcus.tibbitts@dyson.com>
2023-12-01 12:44:14 +0000

    Got console args and file reading working...

--- day01/src/main.js
+++ day01/src/main.js
@@ -1,1 +1,12 @@
-console.log("Advent of code 2023 - Day 01!\r\n");
+var fs = require('fs');
+
+console.log("Advent of code 2023 - Day 01!");
+
+var filename = process.argv[2];
+
+console.log("Input File : " + filename);
+
+fs.readFile(filename, (err, data) => {
+    if (err) throw err;
+    console.log("File Content :\r\n" + data);
+});
