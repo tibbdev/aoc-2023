3349ce50581766cb9d22d617d0f66012686cc875
Marcus Tibbitts <Marcus.Tibbitts@dyson.com>
2023-12-04 18:30:16 +0000

    Day 2 works for example

--- day02/src/main.js
+++ day02/src/main.js
@@ -1,10 +1,68 @@
 var fs = require('fs');
 
+const RED_MAX   = 12;
+const GREEN_MAX = 13;
+const BLUE_MAX  = 14;
+
+let id_acc = 0;
+
 function read_file(path)
 {
-    let file_data = String(fs.readFileSync(String(filename)));
+    let file_data = String(fs.readFileSync(String(path)));
     let split_data = file_data.split("\n");
     return split_data;
+}
+
+function split_game_data(data_line)
+{
+    let blue_max = 0;
+    let red_max = 0;
+    let green_max = 0;
+
+    // Split data into "Game ID; Game Data"
+    let first_split = data_line.split(": ");
+
+    // Get the game ID as a number
+    let id_split = first_split[0].split(" ");
+    let game_id = parseInt(id_split[1]);
+
+    // Split the game into it's rounds
+    let rounds_split = first_split[1].split("; ");
+    rounds_split.forEach(round => {
+        // Process the data for each set of cubes
+        round.split(", ").forEach(cube_info => {
+            let cube_split = cube_info.split(" ");
+            let cube_cnt = parseInt(cube_split[0]);
+            if (cube_info.includes("red"))
+            {
+                if(cube_cnt > red_max)
+                {
+                    red_max = cube_cnt;
+                }
+            }
+            else if (cube_info.includes("green"))
+            {
+                if(cube_cnt > green_max)
+                {
+                    green_max = cube_cnt;
+                }
+            }
+            else if (cube_info.includes("blue"))
+            {
+                if(cube_cnt > blue_max)
+                {
+                    blue_max = cube_cnt;
+                }
+            }
+        });
+    });
+
+    return [game_id, red_max, green_max, blue_max ];
+}
+
+function is_game_possible(game_info)
+{
+    return (game_info[1] <= RED_MAX) && (game_info[2] <= GREEN_MAX) && (game_info[3] <= BLUE_MAX);
 }
 
 console.log("Advent of code 2023 - Day 2!");
@@ -17,6 +75,14 @@ let filename = process.argv[2];
 let file_data = read_file(filename);
 console.log("File Content :\n");
 file_data.forEach(element => {
-    console.log("   " + element);
+    if(element != "") // skip blank lines
+    {
+        let game_data = split_game_data(element);
+        if(is_game_possible(game_data))
+        {
+            id_acc += game_data[0];
+        }
+    }
 });
 
+console.log("Part 1 answer : " + id_acc);
