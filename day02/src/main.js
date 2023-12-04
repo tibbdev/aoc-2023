a89e86bd14478438a44132a93d3042ed773f03ce
Marcus Tibbitts <Marcus.Tibbitts@dyson.com>
2023-12-04 17:59:36 +0000

    Input file reading done

--- day02/src/main.js
+++ day02/src/main.js
@@ -1,1 +1,22 @@
-console.log("Advent of code 2023 - Day 02!\r\n");
+var fs = require('fs');
+
+function read_file(path)
+{
+    let file_data = String(fs.readFileSync(String(filename)));
+    let split_data = file_data.split("\n");
+    return split_data;
+}
+
+console.log("Advent of code 2023 - Day 2!");
+
+// Read filename as extra argument
+let filename = process.argv[2];
+// console.log("Input File : " + filename);
+
+// Read input file
+let file_data = read_file(filename);
+console.log("File Content :\n");
+file_data.forEach(element => {
+    console.log("   " + element);
+});
+
