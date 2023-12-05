e608117dd65589978c2bfe45b81ae8cfedbc6048
M. Tibbitts <marcus.tibbitts@dyson.com>
2023-12-05 14:22:04 +0000

    works for example but blows up with the big numbers in my input file... need to think smarter

--- day05/src/main.js
+++ day05/src/main.js
@@ -2,6 +2,8 @@ var fs = require('fs');
 
 console.log("Advent of code 2023 - Day 5!");
 
+let max_mapping = 0;
+
 function read_file(path)
 {
     let file_data = String(fs.readFileSync(String(path)));
@@ -20,49 +22,161 @@ function get_seeds(data)
     return seeds;
 }
 
-function get_seed_soil_map(data)
+function raw_map_split(str)
 {
-    let seed_soil_map = [];
+    let split_str = str.split(" ");
+    return  {
+                "d_start": parseInt(split_str[0]),
+                "s_start": parseInt(split_str[1]),
+                "range": parseInt(split_str[2]),
+            };
+}
+
+function get_raw_map(data, map_keyword)
+{
+    let map_out = [];
 
     let done = false;
     let started = false;
     data.forEach(line =>
         {
-            if (done)
+            if (!done)
             {
-                console.log("done");
-                return seed_soil_map;
-            }
-            else if(started)
-            {
-                if (line === "")
+                if(started)
                 {
-                    done = true;
+                    if (line === "")
+                    {
+                        done = true;
+                    }
+                    else
+                    {
+                        map_out.push(raw_map_split(line));
+                        console.log("LEN: ", map_out.length);
+                    }
                 }
-                else
+                else if (line.includes(map_keyword))
                 {
-                    console.log("getting map data");
-                    let seed_soil = [];
-                    line.split(" ").forEach(val =>
-                        {
-                            seed_soil.push(parseInt(val));
-                        })
-                    seed_soil_map.push(seed_soil);
+                    started = true;
                 }
-            }
-            else if (line.includes("seed-to-soil"))
-            {
-                console.log("found map start...");
-                started = true;
             }
         });
 
-    return [];
+    return map_out;
+}
+
+function get_seed_soil_map(data)
+{
+    return get_raw_map(data, "seed-to-soil");
+}
+
+function get_soil_fert_map(data)
+{
+    return get_raw_map(data, "soil-to-fertilizer");
+}
+
+function get_fert_water_map(data)
+{
+    return get_raw_map(data, "fertilizer-to-water");
+}
+
+function get_water_light_map(data)
+{
+    return get_raw_map(data, "water-to-light");
+}
+
+function get_light_temp_map(data)
+{
+    return get_raw_map(data, "light-to-temperature");
+}
+
+function get_temp_humid_map(data)
+{
+    return get_raw_map(data, "temperature-to-humidity");
+}
+
+function get_humid_loc_map(data)
+{
+    return get_raw_map(data, "humidity-to-location");
+}
+
+function interpret_mapping(raw_map)
+{
+    let mapping = [];
+
+    // Work out maping values
+    for (let i = 0; i < max_mapping; i++)
+    {
+        let map_entry = {"source": i, "destination": i};
+        mapping.push(map_entry);
+    }
+
+    raw_map.forEach(entry =>
+        {
+            let src_st  = entry.s_start;
+            let rng     = entry.range;
+            let dst_st  = entry.d_start;
+            let cnt = 0;
+            for (let idx = 0; idx < mapping.length; idx++)
+            {
+                if ((src_st <= mapping[idx].source) && ((src_st + rng) > mapping[idx].source))
+                {
+                    mapping[idx].destination = dst_st + cnt;
+                    cnt++;
+                }
+            }
+        });
+
+    return mapping;
+}
+
+function get_destination(map, src)
+{
+    let dst;
+
+    map.forEach(map_entry =>
+        {
+            if(src === map_entry.source)
+            {
+                dst = map_entry.destination;
+            }
+        })
+
+    return dst;
 }
 
 function part1_impl(data)
 {
-    return 0;
+    seeds = get_seeds(file_data);
+
+    console.log("seeds: " + seeds);
+
+    seedsoil_map = interpret_mapping(get_seed_soil_map(file_data));
+    // soilfert_map = interpret_mapping(get_soil_fert_map(file_data));
+    // fertwater_map = interpret_mapping(get_fert_water_map(file_data));
+    // waterlight_map = interpret_mapping(get_water_light_map(file_data));
+    // lighttemp_map = interpret_mapping(get_light_temp_map(file_data));
+    // temphumid_map = interpret_mapping(get_temp_humid_map(file_data));
+    // humidloc_map = interpret_mapping(get_humid_loc_map(file_data));
+
+    let loc_min = max_mapping + 10;
+
+    seeds.forEach(seed => {
+        let soil = get_destination(seedsoil_map, seed);
+        // let fertilizer = get_destination(soilfert_map, soil);
+        // let water = get_destination(fertwater_map, fertilizer);
+        // let light = get_destination(waterlight_map, water);
+        // let temperature = get_destination(lighttemp_map, light);
+        // let humidity = get_destination(temphumid_map, temperature);
+        // let location = get_destination(humidloc_map, humidity);
+        // console.log("seed: " + seed, soil, fertilizer, water, light, temperature, humidity, location);
+        // if (location < loc_min)
+        // {
+        //     loc_min = location;
+        // }
+        loc_min = soil;
+    });
+
+    return loc_min;
 }
 
 function part2_impl(data)
@@ -76,10 +190,28 @@ let filename = process.argv[2];
 // Read input file
 let file_data = read_file(filename);
 
-seeds = get_seeds(file_data);
-seedsoil_map = get_seed_soil_map(file_data);
-console.log(seeds);
-console.log(seedsoil_map);
+file_data.forEach(line =>
+    {
+        if(!line.includes(":"))
+        {
+            let line_split = line.split(" ");
+            if(line_split.length === 3)
+            {
+                let s = parseInt(line_split[0]);
+                let d = parseInt(line_split[1]);
+
+                if(s > d)
+                {
+                    max_mapping = s;
+                }
+                else
+                {
+                    max_mapping = d;
+                }
+            }
+        }
+    });
+
 
 console.log("Part 1 answer : " + part1_impl(file_data));
 console.log("Part 2 answer : " + part2_impl(file_data));
