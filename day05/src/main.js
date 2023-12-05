1a4f7e40725d71214627a8927c343d1e345d0eab
Marcus T. <mtibbitts@pm.me>
2023-12-05 09:06:46 +0000

    Reading seed numbers

--- day05/src/main.js
+++ day05/src/main.js
@@ -8,6 +8,18 @@ function read_file(path)
     let split_data = file_data.split("\n");
     return split_data;
 }
+
+function get_seeds(data)
+{
+    let seeds = [];
+
+    data[0].split(": ")[1].split(" ").forEach(seed_str => {
+        seeds.push(parseInt(seed_str));
+    });
+
+    return seeds;
+}
+
 function part1_impl(data)
 {
     return 0;
@@ -23,6 +35,9 @@ let filename = process.argv[2];
 
 // Read input file
 let file_data = read_file(filename);
+
+seeds = get_seeds(file_data);
+console.log(seeds);
 
 console.log("Part 1 answer : " + part1_impl(file_data));
 console.log("Part 2 answer : " + part2_impl(file_data));
