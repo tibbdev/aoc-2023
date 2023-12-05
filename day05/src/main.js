0eae4ecdc0d9d9ab17ee48cd1bcd13d4c4484dba
Marcus T. <mtibbitts@pm.me>
2023-12-05 09:18:17 +0000

    started getting seed soil map

--- day05/src/main.js
+++ day05/src/main.js
@@ -20,6 +20,46 @@ function get_seeds(data)
     return seeds;
 }
 
+function get_seed_soil_map(data)
+{
+    let seed_soil_map = [];
+
+    let done = false;
+    let started = false;
+    data.forEach(line =>
+        {
+            if (done)
+            {
+                console.log("done");
+                return seed_soil_map;
+            }
+            else if(started)
+            {
+                if (line === "")
+                {
+                    done = true;
+                }
+                else
+                {
+                    console.log("getting map data");
+                    let seed_soil = [];
+                    line.split(" ").forEach(val =>
+                        {
+                            seed_soil.push(parseInt(val));
+                        })
+                    seed_soil_map.push(seed_soil);
+                }
+            }
+            else if (line.includes("seed-to-soil"))
+            {
+                console.log("found map start...");
+                started = true;
+            }
+        });
+
+    return [];
+}
+
 function part1_impl(data)
 {
     return 0;
@@ -37,7 +77,9 @@ let filename = process.argv[2];
 let file_data = read_file(filename);
 
 seeds = get_seeds(file_data);
+seedsoil_map = get_seed_soil_map(file_data);
 console.log(seeds);
+console.log(seedsoil_map);
 
 console.log("Part 1 answer : " + part1_impl(file_data));
 console.log("Part 2 answer : " + part2_impl(file_data));
